f8f234c52b1e434aa71a5c0c8e0db0f9c20eb058
mariellemar <mariellemarsantos@gmail.com>
2023-08-04 11:33:45 -0400

    Adding functions for filtered search

--- newsApp/views.py
+++ newsApp/views.py
@@ -5,10 +5,22 @@ API_KEY = '403d62707f8f4cdf816f7b2e27b4a469'
 
 
 def home(request):
-    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
-    response = requests.get(url)
-    data = response.json()
-    articles = data['articles']
+
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
 
     context = {
         'articles': articles,
